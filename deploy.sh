5f5b944d0db5eb331ac7d5add5c43a7a37fc737d
Aminzy <d1243410203@163.com>
2022-08-04 10:35:45 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -21,8 +21,8 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
+git branch -M main
  git push -f git@github.com:aminzi7/learn-vim.git main:gh-pages
-
 #  cd ../../../ && rm -rf docs/.vitepress/dist
 
 cd -
